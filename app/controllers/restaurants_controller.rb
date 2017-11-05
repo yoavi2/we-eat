8f9b74670095be93df7cabd08d61995b5c096bcf
Yoav Codish <yoav.codish@gmail.com>
2017-11-05 18:44:29 +0200

    When fetching a restaurant - Fetch all it's reviews for db efficiency

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,7 +4,7 @@ class RestaurantsController < ApplicationController
   # GET /restaurants
   # GET /restaurants.json
   def index
-    @restaurants = Restaurant.all
+    @restaurants = Restaurant.includes(:reviews).all
   end
 
   # GET /restaurants/1

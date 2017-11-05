bf137a8a614731c0a4b8e91c669f62a9f8e6f6e5
Yoav Codish <yoav.codish@gmail.com>
2017-11-05 18:05:25 +0200

    Some validations for review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+  validates :rating, presence: true
+  validates :rating, numericality: { greater_than_or_equal_to: 0.00,
+                                     less_than_or_equal_to: 3 }
+  attribute :reviewer, :string, default: 'Anonymous'
+
 end

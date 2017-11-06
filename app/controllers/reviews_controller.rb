031649bb36b9f5dbfc2f312df5cdb0c1c140dbb3
Yoav Codish <yoav.codish@gmail.com>
2017-11-06 16:33:47 +0200

    Disable htmls for reviews (Which aren't yet working) and hopefully fixed review_params

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -27,10 +27,10 @@ class ReviewsController < ApplicationController
 
     respond_to do |format|
       if @review.save
-        format.html { redirect_to restaurant_review_path(@restaurant), notice: 'Review was successfully created.' }
+        # format.html { redirect_to restaurant_review_path(@restaurant), notice: 'Review was successfully created.' }
         format.json { render :show, status: :created, location: @review }
       else
-        format.html { render :new }
+        # format.html { render :new }
         format.json { render json: @review.errors, status: :unprocessable_entity }
       end
     end
@@ -41,10 +41,10 @@ class ReviewsController < ApplicationController
   def update
     respond_to do |format|
       if @review.update(review_params)
-        format.html { redirect_to @review, notice: 'Review was successfully updated.' }
+        # format.html { redirect_to @review, notice: 'Review was successfully updated.' }
         format.json { render :show, status: :ok, location: @review }
       else
-        format.html { render :edit }
+        # format.html { render :edit }
         format.json { render json: @review.errors, status: :unprocessable_entity }
       end
     end
@@ -55,7 +55,7 @@ class ReviewsController < ApplicationController
   def destroy
     @review.destroy
     respond_to do |format|
-      format.html { redirect_to reviews_url, notice: 'Review was successfully destroyed.' }
+      # format.html { redirect_to reviews_url, notice: 'Review was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -71,6 +71,6 @@ class ReviewsController < ApplicationController
   end
 
   def review_params
-    params.fetch(:review, {})
+    params.require(:review).permit(:reviewer_name, :rating, :comment)
   end
 end

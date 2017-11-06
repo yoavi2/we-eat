3a087c134858371677a48bd21ad85c44d10e9024
Yoav Codish <yoav.codish@gmail.com>
2017-11-06 11:44:19 +0200

    Update reviews controller to work with restaurants

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,34 +1,33 @@
 class ReviewsController < ApplicationController
-  before_action :set_review, only: [:show, :edit, :update, :destroy]
+  before_action :set_review, only: %i[show edit update destroy]
+  before_action :set_restaurant, only: %i[index new create]
 
-  # GET /reviews
-  # GET /reviews.json
+  # GET restaurant/1/reviews
+  # GET restaurant/1/reviews.json
   def index
-    @reviews = Review.all
+    @reviews = @restaurant.reviews
   end
 
-  # GET /reviews/1
-  # GET /reviews/1.json
-  def show
-  end
+  # GET restaurant/1/reviews/1
+  # GET restaurant/1/reviews/1.json
+  def show; end
 
-  # GET /reviews/new
+  # GET restaurant/1/reviews/new
   def new
-    @review = Review.new
+    @review = @restaurant.reviews.new
   end
 
-  # GET /reviews/1/edit
-  def edit
-  end
+  # GET restaurant/1/reviews/1/edit
+  def edit; end
 
-  # POST /reviews
-  # POST /reviews.json
+  # POST restaurant/1/reviews
+  # POST restaurant/1/reviews.json
   def create
-    @review = Review.new(review_params)
+    @review = @restaurant.reviews.create(review_params)
 
     respond_to do |format|
       if @review.save
-        format.html { redirect_to @review, notice: 'Review was successfully created.' }
+        format.html { redirect_to restaurant_review_path(@restaurant), notice: 'Review was successfully created.' }
         format.json { render :show, status: :created, location: @review }
       else
         format.html { render :new }
@@ -37,8 +36,8 @@ class ReviewsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /reviews/1
-  # PATCH/PUT /reviews/1.json
+  # PATCH/PUT restaurant/1/reviews/1
+  # PATCH/PUT restaurant/1/reviews/1.json
   def update
     respond_to do |format|
       if @review.update(review_params)
@@ -51,8 +50,8 @@ class ReviewsController < ApplicationController
     end
   end
 
-  # DELETE /reviews/1
-  # DELETE /reviews/1.json
+  # DELETE restaurant/1/reviews/1
+  # DELETE restaurant/1/reviews/1.json
   def destroy
     @review.destroy
     respond_to do |format|
@@ -62,13 +61,16 @@ class ReviewsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_review
-      @review = Review.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def review_params
-      params.fetch(:review, {})
-    end
+  def set_review
+    @review = Review.find(params[:id])
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.fetch(:review, {})
+  end
 end

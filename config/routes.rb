02e2c8bd44aba98e6febadbebbecace9e6d3028d
Yoav Codish <yoav.codish@gmail.com>
2017-11-06 16:32:57 +0200

    Fixed root route to point to restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :restaurants do
     resources :reviews
-
-    root 'restaurants#index'
   end
+  
+  root 'restaurants#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

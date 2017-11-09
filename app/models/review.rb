fecd86bd3f94bacdaa6aeb72c31a5c6aebe82882
Yoav Codish <yoav.codish@gmail.com>
2017-11-09 18:08:17 +0200

    Fixing review model according to tests :)

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant, dependent: :destroy
-  validates :rating, presence: true
+  validates :reviewer_name, :rating, presence: true
   validates :rating, numericality: { greater_than_or_equal_to: 0.00,
                                      less_than_or_equal_to: 3 }
   attribute :reviewer_name, :string, default: 'Anonymous'

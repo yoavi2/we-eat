f4287f381b6ee27ad1593e1791c549cdfeadd52d
Yoav Codish <yoav.codish@gmail.com>
2017-11-15 11:24:49 +0200

    Add delivery time to restaurant component

--- app/javascript/app/components/restaurant.jsx
+++ app/javascript/app/components/restaurant.jsx
@@ -11,7 +11,9 @@ const Restaurant = ({ restaurant }) => (
     <h2> {restaurant.name} </h2>
     <Rating rating={restaurant.rating} maxRating={3} />
     <TenBisIcon isTenBis={restaurant.is_10_bis}/>
-
+    { restaurant.max_deliver_in_min !== null &&
+      <h4> Deliver in: {restaurant.max_deliver_in_min}</h4>
+    }
   </div>
 );
 
